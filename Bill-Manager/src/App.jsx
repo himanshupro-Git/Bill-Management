14cc14c8ec858e753b38bab4e936134426cb15fc
Himanshu Kumar <himanshu.learner.pro@gmail.com>
2026-08-20 19:25:44 +0530

    Fix subtotal calculation to use item quantity

--- Bill-Manager/src/App.jsx
+++ Bill-Manager/src/App.jsx
@@ -15,8 +15,8 @@ const [suggestion, setSuggestion] = useState([]);
 
 
 const itemCount = items.reduce((sum, item)=>sum + item.quantity,0);
-const subtotal = items.reduce((sum, item)=>sum + item.price*itemCount, 0);
-const tax = subtotal*0.30 ;
+const subtotal = items.reduce((sum, item)=>sum + item.price*item.quantity, 0);
+const tax = subtotal*0.30;
 const total = subtotal + tax;
 
 
